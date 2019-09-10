524694cb53eb90c3baeba849eadb1b9e50a1b11c
Facundo Diaz Pascielli <facupascielli@gmail.com>
2019-09-10 16:25:41 -0300

    Tarea terminada

--- Clase 6/Arrays paralelos/main.c
+++ Clase 6/Arrays paralelos/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 1
+#define TAM 3
 
 void mostrarAlumnos(int leg[], int ed[], char sex[], int n1[], int n2[], float prom[], int tam);
 void mostrarAlumno(int leg, int ed, char sex, int n1, int n2, float prom);
-//armar funcion ordenar alumnos por legajo
+void ordenarVectorInt(int vec[], int tam, int criterio);
+void ordenarAlumnosXLegajo(int leg[], int ed[], char sex[], int n1[], int n2[], float prom[], int tam, int criterio);
+
 int main()
 {
     int legajos[TAM];
@@ -45,7 +47,7 @@ int main()
 
             printf("\nIngrese nota del primer parcial(1-10): ");
             scanf("%d", &notaP1[i]);
-            while(notaP1[i] < 0 || notaP1[i] > 10)
+            while(notaP1[i] < 1 || notaP1[i] > 10)
                 {
                     printf("\nError. Ingrese nota del primer parcial (1 - 10): ");
                     scanf("%d", &notaP1[i]);
@@ -53,7 +55,7 @@ int main()
 
             printf("\nIngrese nota del segundo parcial(1-10): ");
             scanf("%d", &notaP2[i]);
-            while(notaP2[i] < 0 || notaP2[i] > 10)
+            while(notaP2[i] < 1 || notaP2[i] > 10)
                 {
                     printf("\nError. Ingrese nota del segundo parcial (1 - 10): ");
                     scanf("%d", &notaP2[i]);
@@ -61,6 +63,7 @@ int main()
 
             promedios[i] = (float) (notaP1[i] + notaP2[i]) /2;
         }
+    ordenarAlumnosXLegajo(legajos, edades, sexos, notaP1, notaP2, promedios, TAM, 0);
     mostrarAlumnos(legajos, edades, sexos, notaP1, notaP2, promedios, TAM);
 
     return 0;
@@ -71,7 +74,7 @@ void mostrarAlumnos(int leg[], int ed[], char sex[], int n1[], int n2[], float p
     printf("Legajo  Edad  Sexo  Nota 1  Nota 2  Promedios \n\n");
     for(int i = 0; i < tam; i++)
         {
-            printf("%d  %d  %c  %d  %d  %.2f\n", leg[i], ed[i], sex[i], n1[i], n2[i], prom[i]);
+            mostrarAlumno(leg[i], ed[i], sex[i], n1[i], n2[i], prom[i]);
         }
     printf("\n\n");
 }
@@ -79,4 +82,75 @@ void mostrarAlumnos(int leg[], int ed[], char sex[], int n1[], int n2[], float p
 void mostrarAlumno(int leg, int ed, char sex, int n1, int n2, float prom)
 {
     printf("%d  %d  %c  %d  %d  %.2f\n", leg, ed, sex, n1, n2, prom);
+}
+
+void ordenarAlumnosXLegajo(int leg[], int ed[], char sex[], int n1[], int n2[], float prom[], int tam, int criterio)
+{
+    int aux;
+
+    for(int i = 0; i < tam-1; i++)
+        {
+            for(int j = i + 1; j < tam; j++)
+                {
+                    if(criterio == 0)
+                        {
+                            if(leg[j] < leg[i])
+                            {
+                                aux = leg[j];
+                                leg[j] = leg[i];
+                                leg[i] = aux;
+
+                                aux = ed[j];
+                                ed[j] = ed[i];
+                                ed[i] = aux;
+
+                                aux = sex[j];
+                                sex[j] = sex[i];
+                                sex[i] = aux;
+
+                                aux = n1[j];
+                                n1[j] = n1[i];
+                                n1[i] = aux;
+
+                                aux = n2[j];
+                                n2[j] = n2[i];
+                                n2[i] = aux;
+
+                                aux = prom[j];
+                                prom[j] = prom[i];
+                                prom[i] = aux;
+
+                            }
+                        }else
+                        {
+                            if(leg[j] > leg[i])
+                            {
+                                aux = leg[j];
+                                leg[j] = leg[i];
+                                leg[i] = aux;
+
+                                aux = ed[j];
+                                ed[j] = ed[i];
+                                ed[i] = aux;
+
+                                aux = sex[j];
+                                sex[j] = sex[i];
+                                sex[i] = aux;
+
+                                aux = n1[j];
+                                n1[j] = n1[i];
+                                n1[i] = aux;
+
+                                aux = n2[j];
+                                n2[j] = n2[i];
+                                n2[i] = aux;
+
+                                aux = prom[j];
+                                prom[j] = prom[i];
+                                prom[i] = aux;
+                            }
+                        }
+
+                }
+        }
 }
