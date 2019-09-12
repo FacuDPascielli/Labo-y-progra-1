a87eb3b8e333981a72297c3a34eaf3bd9a7a0409
Facudpascielli <facupascielli@gmail.com>
2019-09-11 23:28:56 -0300

    Ejercicio clase 7 terminado

--- Clase 7/Cadenas 2/main.c
+++ Clase 7/Cadenas 2/main.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void mostrarNombres(char mat [][20]);//Terminar esta funcion y hacer una funcion que ordene alfabéticamente los nombres.
+void ordenarMatChar(char mat[][20], int tam);
 
 int main()
 {
     char nombres[5][20];
 
     for(int i = 0; i < 5; i++)
-        {
-            printf("Ingrese un nombre: ");
-            fflush(stdin);
-            gets(nombres[i]);
-        }
+    {
+        printf("Ingrese un nombre: ");
+        fflush(stdin);
+        gets(nombres[i]);
+    }
 
     printf("\n\n");
 
-    for(int i = 0; i < 5; i++)
-        {
-            printf("%s\n", nombres[i]);
-        }
+    printf("Nombres sin ordenar: \n");
+    mostrarNombres(nombres);
+
+    ordenarMatChar(nombres, 5);
+    printf("Nombres ordenados: \n");
+    mostrarNombres(nombres);
+
     return 0;
+}
+void mostrarNombres(char mat [][20])
+{
+    for(int i = 0; i < 5; i++)
+    {
+        printf("\n%s\n", mat[i]);
+    }
+    printf("\n\n");
+}
+
+void ordenarMatChar(char mat[][20], int tam)
+{
+    char aux[20];
+    for(int i = 0; i < tam-1; i++)
+    {
+        for(int j = i + 1; j < tam; j++)
+        {
+            if(strcmp(mat[i], mat[j]) == 1)
+            {
+                strcpy(aux, mat[j]);
+                strcpy(mat[j], mat[i]);
+                strcpy(mat[i], aux);
+            }
+
+
+        }
+    }
 }
